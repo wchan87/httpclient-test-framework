85f46db9cf05f83e07012191130253bc8538e216
Wren <wlechan87@gmail.com>
2017-12-16 19:05:37 -0500

    Cleaned up code for proxy test

--- src/test/java/edu/nyu/BasicProxyHttpClientTest.java
+++ src/test/java/edu/nyu/BasicProxyHttpClientTest.java
@@ -1,5 +1,6 @@
 package edu.nyu;
 
+import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -22,6 +23,8 @@ import org.junit.Test;
 import org.littleshoot.proxy.HttpProxyServer;
 import org.littleshoot.proxy.ProxyAuthenticator;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -33,6 +36,10 @@ public class BasicProxyHttpClientTest {
     private static final int PORT = 12345;
     private static final int PROXY_PORT = 54321;
     private static final String ROOT_CONTEXT = "/";
+    private static final String USER = "user";
+    private static final String PASS = "pass";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BasicProxyHttpClientTest.class);
 
     private HttpProxyServer proxyHttpServer;
     private HttpServer httpServer;
@@ -44,19 +51,10 @@ public class BasicProxyHttpClientTest {
     }
 
     @Test
-    public void testProxyAuthenticationRequired() throws IOException, URISyntaxException {
+    public void testProxy() throws IOException, URISyntaxException {
         // setup proxy server
         proxyHttpServer = DefaultHttpProxyServer.bootstrap()
-                .withPort(PROXY_PORT).withProxyAuthenticator(new ProxyAuthenticator() {
-                    @Override
-                    public boolean authenticate(String s, String s1) {
-                        return true;
-                    }
-                    @Override
-                    public String getRealm() {
-                        return null;
-                    }
-                }).start();
+                .withPort(PROXY_PORT).start();
         // setup embedded server
         httpServer = ServerBootstrap.bootstrap()
                 .setListenerPort(PORT)
@@ -65,8 +63,27 @@ public class BasicProxyHttpClientTest {
         httpServer.start();
 
         // setup HttpClient
-        HttpClient httpClient = HttpClientBuilder.create().setRoutePlanner(
-                new DefaultProxyRoutePlanner(new HttpHost(LOCALHOST, PROXY_PORT))).build();
+        HttpClient httpClient = HttpClientBuilder.create().setProxy(new HttpHost(LOCALHOST, PROXY_PORT)).build();
+        HttpUriRequest httpUriRequest = RequestBuilder.get(new URIBuilder().setScheme(HTTP_SCHEME)
+                .setHost(LOCALHOST).setPort(PORT).setPath(ROOT_CONTEXT).build()).build();
+        HttpResponse httpResponse = httpClient.execute(httpUriRequest);
+        Assert.assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
+    }
+
+    @Test
+    public void testProxyAuthenticationRequired() throws IOException, URISyntaxException {
+        // setup proxy server
+        proxyHttpServer = DefaultHttpProxyServer.bootstrap()
+                .withPort(PROXY_PORT).withProxyAuthenticator(getProxyAuthenticator()).start();
+        // setup embedded server
+        httpServer = ServerBootstrap.bootstrap()
+                .setListenerPort(PORT)
+                .registerHandler(ROOT_CONTEXT, (req, resp, context) -> resp.setStatusCode(HttpStatus.SC_OK))
+                .create();
+        httpServer.start();
+
+        // setup HttpClient
+        HttpClient httpClient = HttpClientBuilder.create().setProxy(new HttpHost(LOCALHOST, PROXY_PORT)).build();
         HttpUriRequest httpUriRequest = RequestBuilder.get(new URIBuilder().setScheme(HTTP_SCHEME)
                 .setHost(LOCALHOST).setPort(PORT).setPath(ROOT_CONTEXT).build()).build();
         HttpResponse httpResponse = httpClient.execute(httpUriRequest);
@@ -77,20 +94,17 @@ public class BasicProxyHttpClientTest {
     public void testProxyAuthenticationSuccess() throws IOException, URISyntaxException {
         // setup proxy server
         proxyHttpServer = DefaultHttpProxyServer.bootstrap()
-                .withPort(PROXY_PORT).withProxyAuthenticator(new ProxyAuthenticator() {
-                    @Override
-                    public boolean authenticate(String s, String s1) {
-                        return true;
-                    }
-                    @Override
-                    public String getRealm() {
-                        return null;
-                    }
-                }).start();
+                .withPort(PROXY_PORT).withProxyAuthenticator(getProxyAuthenticator()).start();
         // setup embedded server
         httpServer = ServerBootstrap.bootstrap()
                 .setListenerPort(PORT)
-                .registerHandler(ROOT_CONTEXT, (req, resp, context) -> resp.setStatusCode(HttpStatus.SC_OK))
+                .registerHandler(ROOT_CONTEXT, (req, resp, context) -> {
+                    for (Header h : req.getAllHeaders()) {
+                        // log the request headers to prove that Proxy-Authorization header isn't leaked
+                        LOGGER.info("Request Header >> {} : {}", h.getName(), h.getValue());
+                    }
+                    resp.setStatusCode(HttpStatus.SC_OK);
+                })
                 .create();
         httpServer.start();
 
@@ -98,15 +112,27 @@ public class BasicProxyHttpClientTest {
         // https://stackoverflow.com/questions/6962047/apache-httpclient-4-1-proxy-authentication
         CredentialsProvider credsProvider = new BasicCredentialsProvider();
         credsProvider.setCredentials(new AuthScope(LOCALHOST, PROXY_PORT),
-                new UsernamePasswordCredentials("user", "pass"));
-        HttpClient httpClient = HttpClientBuilder.create().setRoutePlanner(
-                new DefaultProxyRoutePlanner(new HttpHost(LOCALHOST, PROXY_PORT)))
+                new UsernamePasswordCredentials(USER, PASS));
+        HttpClient httpClient = HttpClientBuilder.create().setProxy(new HttpHost(LOCALHOST, PROXY_PORT))
                 .setDefaultCredentialsProvider(credsProvider)
                 .setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy()).build();
         HttpUriRequest httpUriRequest = RequestBuilder.get(new URIBuilder().setScheme(HTTP_SCHEME)
                 .setHost(LOCALHOST).setPort(PORT).setPath(ROOT_CONTEXT).build()).build();
         HttpResponse httpResponse = httpClient.execute(httpUriRequest);
         Assert.assertEquals(httpResponse.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
+    }
+
+    private ProxyAuthenticator getProxyAuthenticator() {
+        return new ProxyAuthenticator() {
+            @Override
+            public boolean authenticate(String user, String pass) {
+                 return user.equals(USER) && pass.equals(PASS);
+            }
+            @Override
+            public String getRealm() {
+                return null;
+            }
+        };
     }
 
 }
